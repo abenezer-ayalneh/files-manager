cd5d0103373cfa9293bb45a1c930f2bfe08302c5
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-12-05 11:48:47 +0300

    feat: implement password hashing with bcrypt
    - Add bcrypt hashing abstract class and concrete implementation
    
    Signed-off-by: abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
+import { IamModule } from './iam/iam.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+    IamModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
