e5c1bc81c3800ef03f5c468936fe1fb3b0f53f06
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-12-06 12:10:59 +0300

    fix: add prisma module
    Signed-off-by: abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { UsersModule } from './users/users.module'
 import { IamModule } from './iam/iam.module'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, IamModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+    IamModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
