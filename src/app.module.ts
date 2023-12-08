03ab2975a13c65299bb8c43dbb632be539585df0
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-12-08 09:32:38 +0300

    fix: remove unused module user
    Signed-off-by: abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
-import { UsersModule } from './users/users.module'
 import { IamModule } from './iam/iam.module'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    UsersModule,
-    IamModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, IamModule],
   controllers: [AppController],
   providers: [AppService],
 })
