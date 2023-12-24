fb7e36ce905f234b9a1acc91b729261d6049a3a2
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2023-12-24 12:09:58 +0300

    test: implement test case for sign up method

--- src/iam/authentication/authentication.service.spec.ts
+++ src/iam/authentication/authentication.service.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthenticationService } from './authentication.service'
 import { PrismaService } from '../../prisma/prisma.service'
-import { HashingService } from '../hashing/hashing.service'
 import { JwtService } from '@nestjs/jwt'
-import process from 'process'
-import jwtConfig from "../config/jwt.config";
-import { RefreshTokenIdsStorage } from "./refresh-token-ids.storage/refresh-token-ids.storage";
+import jwtConfig from '../config/jwt.config'
+import { RefreshTokenIdsStorage } from './refresh-token-ids.storage/refresh-token-ids.storage'
+import { SignUpDto } from './dto/sign-up.dto/sign-up.dto'
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended'
+import { PrismaClient, User } from '@prisma/client'
+import { HashingService } from '../hashing/hashing.service'
+import { AuthenticationController } from "./authentication.controller";
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService
-  let prisma: PrismaService
-  const mockHashingService = {
+  let prisma: DeepMockProxy<PrismaClient>
+  const mockBcryptService = {
     hash: jest.fn(),
     compare: jest.fn(),
   }
@@ -36,18 +39,54 @@ describe('AuthenticationService', () => {
       providers: [
         AuthenticationService,
         PrismaService,
-        { provide: HashingService, useValue: mockHashingService },
+        { provide: HashingService, useValue: mockBcryptService },
         { provide: JwtService, useValue: mockJwtService },
         { provide: jwtConfig.KEY, useValue: mockJwtConfiguration },
-        { provide: RefreshTokenIdsStorage, useValue: mockRefreshTokenIdsStorage },
+        {
+          provide: RefreshTokenIdsStorage,
+          useValue: mockRefreshTokenIdsStorage,
+        },
       ],
-    }).compile()
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile()
 
     service = module.get<AuthenticationService>(AuthenticationService)
-    prisma = module.get<PrismaService>(PrismaService)
+    prisma = module.get<DeepMockProxy<PrismaClient>>(PrismaService)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('signUp => Should create a user', async () => {
+    // Arrange
+    const signUpDto = {
+      email: 'john.doe@gmail.com',
+      password: 'passpass',
+    } as SignUpDto
+    const hashedPassword = 'this-is-a-hashed-password'
+    const user = {
+      id: 1,
+      email: signUpDto.email,
+      password: hashedPassword,
+    } as User
+
+    jest.spyOn(mockBcryptService, 'hash').mockReturnValue(hashedPassword)
+    // @ts-ignore
+    prisma.user.create.mockReturnValue(user)
+
+    // Act
+    const result = await service.signUp(signUpDto)
+
+    // Assert
+    expect.assertions(4)
+    expect(mockBcryptService.hash).toHaveBeenCalledTimes(1)
+    expect(mockBcryptService.hash).toHaveBeenCalledWith(signUpDto.password)
+    expect(prisma.user.create).toHaveBeenCalledTimes(1)
+    expect(prisma.user.create).toHaveBeenCalledWith({
+      data: { email: signUpDto.email, password: hashedPassword },
+    })
   })
 })
